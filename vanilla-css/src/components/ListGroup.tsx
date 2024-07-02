56aff58e49592da979e7a00265114362b9a0d00c
Tarun Khurana <tarunkhurana1982@gmail.com>
2024-07-02 12:52:38 -0500

    vanilla css initial setup

---
+++ vanilla-css/src/components/ListGroup.tsx
@@ -0,0 +1,36 @@
+import React, { useState } from "react";
+
+interface Props {
+  items: string[];
+  heading: string;
+  onSeletItem: (item: string) => void;
+}
+
+const ListGroup = ({ items, heading, onSeletItem }: Props) => {
+  const [selectedIndex, setSelectedIndex] = useState(-1);
+  return (
+    <>
+      <h1>{heading}</h1>
+      <ul className="list-group">
+        {items.map((item, index) => (
+          <li
+            className={
+              selectedIndex === index
+                ? "list-group-item active"
+                : "list-group-item"
+            }
+            key={item}
+            onClick={() => {
+              setSelectedIndex(index);
+              onSeletItem(item);
+            }}
+          >
+            {item}
+          </li>
+        ))}
+      </ul>
+    </>
+  );
+};
+
+export default ListGroup;
