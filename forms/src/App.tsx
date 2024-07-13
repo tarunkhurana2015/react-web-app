e39497c99009a9eb02fa232b38cb42a6cb262c02
Tarun Khurana <tarunkhurana1982@gmail.com>
2024-07-13 12:08:33 -0500

    small refactor

--- forms/src/App.tsx
+++ forms/src/App.tsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import reactLogo from "./assets/react.svg";
-import Form from "./form";
+import Form from "./FormWithUseRef";
 import FormWithStateHook from "./FormWithStateHook";
 import FormWithReactFormHook from "./FormWithReactFormHook";
 import FormSchemaBasedValidation from "./FormSchemaBasedValidation";
